400105c4af6a7774f984eac34b8cb29bc49d9665
Jason Cardenas <jasonc62492@gmail.com>
2024-07-30 13:42:48 -0400

    logging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,18 @@
-import { DocumentData } from "firebase-admin/firestore";
+// import { DocumentData } from "firebase-admin/firestore";
+const functions = require("firebase-functions");
 import {
     onDocumentWritten,
-    Change,
-    FirestoreEvent
 } from "firebase-functions/v2/firestore";
 
-exports.behavior_written = onDocumentWritten("behaviors/{behaviorId}", (event) => {
+exports.behavior_written = onDocumentWritten("behaviors/{behaviorId}", (event: any) => {
     const behaviorId = event.params.behaviorId;
+    const before = event.data.before.data()
+    const after = event.data.after.data()
+    try {
+        functions.logger.info("behavior_written", behaviorId);
+        functions.logger.info("before", before);
+        functions.logger.info("after", after);
+    } catch (error) {
+        throw new functions.https.HttpsError("failed-precondition", error);
+    }
 });
--- functions/src/langchain_helpers/index.ts
+++ functions/src/langchain_helpers/index.ts
@@ -1,3 +1,6 @@
+import { functions } from "firebase-functions";
+
 export async function formatBehavior(behavior: any) {
     const behaviorText = behavior.data();
+    functions.logger.log(behaviorText);
 }
